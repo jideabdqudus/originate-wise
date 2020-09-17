ee3563d26da36187b80378b8555fbb130c9b10f7
jideabdqudus <jideabdqudus@gmail.com>
2020-09-17 13:22:24 +0100

    Added Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+//import { Route, BrowserRouter, Switch } from "react-router-dom";
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Hello world</h1>
+    </Fragment>
+  );
+};
+ //dssds
+export default App;
+
+// <Fragment>
+//       <BrowserRouter>
+//         <Switch></Switch>
+//       </BrowserRouter>
+//     </Fragment>
