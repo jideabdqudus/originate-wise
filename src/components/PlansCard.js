b2644874c948d4087f1e5e541695205ac509df42
jideabdqudus <jideabdqudus@gmail.com>
2020-09-23 10:49:10 +0100

    Added Commit

--- src/components/PlansCard.js
+++ src/components/PlansCard.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Card, Badge } from "antd";
 
 const PlansCard = () => {
-    return (
-        <div>
-            <h1>Hello Bro</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Row>
+        <Col span={9} className="dashMaturityCard">
+          <Badge.Ribbon text="REGULAR PLAN MATURITY" title="Amazing Deals">
+            <Card
+              className="cardHero"
+              style={{ backgroundColor: "#e1f6ff", margin: 0 }}
+            >
+              <p style={{ fontSize: "15px", fontWeight: "500" }}>
+                New House Purchase
+              </p>
+              <h2
+                style={{
+                  fontSize: "2.4rem",
+                  fontWeight: "600",
+                  color: "#0a2e65",
+                }}
+              >
+                <sup
+                  style={{
+                    fontSize: "1.2rem",
+                    fontWeight: "500",
+                    top: "-1em",
+                    marginRight: "4px",
+                  }}
+                >
+                  ₦
+                </sup>
+                247,900
+                <sub
+                  style={{
+                    bottom: ".02em",
+                    marginLeft: "4px",
+                    fontWeight: "700",
+                    opacity: ".6",
+                    fontSize: ".3em",
+                  }}
+                >
+                  .00
+                </sub>
+              </h2>
+              <hr />
+            </Card>
+          </Badge.Ribbon>
+        </Col>
+      </Row>
+      <h1
+        style={{
+          color: "rgba(10,46,101)",
+          opacity: ".3",
+          marginTop: "50px",
+          fontSize: "40px",
+          fontWeight: "600",
+        }}
+      >
+        Add new Plan
+      </h1>
+    </div>
+  );
+};
 
-export default PlansCard
+export default PlansCard;
