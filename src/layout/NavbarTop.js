285675ce3932f39fe5dffac4c744f3ed087f9972
jideabdqudus <jideabdqudus@gmail.com>
2020-10-03 16:27:16 +0100

    Added Commit

--- src/layout/NavbarTop.js
+++ src/layout/NavbarTop.js
@@ -2,16 +2,27 @@ import React from "react";
 import { PageHeader, Button } from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../actions/authActions";
 
-const NavbarTop = () => {
+const NavbarTop = ({ logout, user }) => {
+  const onClick = () => {
+    console.log("working");
+  };
+
   return (
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
         ghost={false}
         title="Howdy"
-        subTitle="Jide Abdul-Qudus"
+        subTitle={`${user.firstname} ${user.lastname}`}
         extra={[
-          <Button key="1" type="primary" style={{ borderRadius: "30px" }}>
+          <Button
+            key="1"
+            type="primary"
+            style={{ borderRadius: "30px" }}
+            onClick={onClick}
+          >
             <LogoutOutlined
               style={{
                 fontSize: 20,
@@ -25,4 +36,12 @@ const NavbarTop = () => {
   );
 };
 
-export default NavbarTop;
+NavbarTop.propTypes = {
+  logout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.authReducer.user,
+});
+
+export default connect(mapStateToProps, { logout })(NavbarTop);
